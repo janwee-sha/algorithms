bcfa5c8098bc05fd991efb652f2fd9a1e43ded08
janwee-sha <janwee_sha@outlook.com>
2022-08-26 19:42:15 +0800

    Java

--- src/main/java/tree/BTree.java
+++ src/main/java/tree/BTree.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.List;
+
 /**
  * An implementation of B-Tree
  *
@@ -7,13 +9,58 @@ package tree;
  * @param <V> value
  */
 public class BTree<K extends Comparable<K>, V> implements Tree<K, V> {
+    private Node<K, V> root;
+    private int minDegree;
+
+    public BTree(Node<K, V> root, int minDegree) {
+        this.root = root;
+        this.minDegree = minDegree;
+    }
+
     @Override
     public V get(K key) {
-        return null;
+        return get(this.root, key);
+    }
+
+    public V get(Node<K, V> node, K key) {
+        List<Node.Pair<K, V>> pairs = node.pairs;
+        int i = 0;
+        while (i < pairs.size() && key.compareTo(pairs.get(i).key) > 0) {
+            i++;
+        }
+        Node.Pair<K, V> pair = pairs.get(i);
+        if (key == pair.key) {
+            return pair.val;
+        } else if (node.isLeaf) {
+            return null;
+        } else {
+            return get(node.children.get(i), key);
+        }
     }
 
     @Override
     public void insert(K key, V val) {
 
+    }
+
+    private void splitChildren(Node<K, V> nonfullNode, int i) {
+
+    }
+
+    private static class Node<K extends Comparable<K>, V> {
+        private BTree<K, V> owner;
+        private List<Pair<K, V>> pairs;
+        private List<Node<K, V>> children;
+        private boolean isLeaf;
+
+        private static class Pair<K extends Comparable<K>, V> {
+            private K key;
+            private V val;
+
+            public Pair(K key, V val) {
+                this.key = key;
+                this.val = val;
+            }
+        }
     }
 }
